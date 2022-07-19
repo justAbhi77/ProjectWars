94b0231b3e69171b72fbde4e231b976fd367579a
justAbhi77 <ojhaabhinav2016@gmail.com>
2022-07-19 11:50:11 +0530

    Added functionality to the menu items and added the a new scene for the game

---
+++ Assets/Scripts/Menu_Manager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Manager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject main_menu,options_menu;
+
+    void Start()
+    {
+        options_menu.SetActive(false);
+        main_menu.SetActive(true);
+    }
+
+    public void Startbtn()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    /// <summary>
+    /// Flips the Gameobject from active to inactive;
+    /// </summary>
+    public void optionsbtn()
+    {
+        // activeSelf returns the local active state of this GameObject.
+        // Since the gameobjects here are all Parents so both activeSelf and activeInHierarchy work the same here.
+        options_menu.SetActive(!options_menu.activeSelf);
+        main_menu.SetActive(!main_menu.activeSelf);
+
+        // see Unity Manual for further explaination on GameObject.activeSelf
+    }
+
+    /// <summary>
+    /// Quit the application entirely.
+    /// </summary>
+    public void quit()
+    {
+        Debug.Log("Quitting Application");
+        Application.Quit();
+    }
+
+}
