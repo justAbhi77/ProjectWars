eabaee5a67e6ecbc9e0b9bd2d70c85c17c5b0c9c
justAbhi77 <ojhaabhinav2016@gmail.com>
2022-07-19 11:48:51 +0530

    Added a simple mouse over to the ui buttons on main menu.

---
+++ Assets/Scripts/OnMouseOver.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Changes the mouse pointer when it enters/exits the button.
+/// It implements two other interfaces, other than MonoBehaviour , in order to achieve this, namely,IPointerEnterHandler and IPointerExitHandler.
+/// </summary>
+public class OnMouseOver : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    /// <summary>
+    /// The texture the pointer is changed to.
+    /// </summary>
+    [SerializeField]
+    Texture2D mouse_tex;
+
+    /// <summary>
+    /// Function called when the pointer enters the button.
+    /// </summary>
+    /// <param name="eventData">
+    /// The data sent by the EventSystems namespace.
+    /// </param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(mouse_tex, Vector2.zero, CursorMode.Auto);
+    }
+
+
+    /// <summary>
+    /// Function called when the pointer exits the button.
+    /// </summary>
+    /// <param name="eventData">
+    /// The data sent by the EventSystems namespace.
+    /// </param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
